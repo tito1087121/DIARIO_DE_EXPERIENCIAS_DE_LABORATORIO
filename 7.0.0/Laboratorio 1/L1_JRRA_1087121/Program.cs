573454e3f16f6107ed23b043ba815b26a754a0a9
tito1087121 <111389124+tito1087121@users.noreply.github.com>
2022-09-25 14:23:33 -0600

    Add files via upload

---
+++ 7.0.0/Laboratorio 1/L1_JRRA_1087121/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace L1_JRRA_1087121
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese su nombre: ");
+            String Nombre = Console.ReadLine();
+
+            Console.WriteLine(""); 
+            Console.WriteLine("Hola Mundo ");
+            Console.WriteLine("soy " + Nombre);
+            Console.WriteLine("");
+
+            //La diferencia entre writeline y write es que con la instrucción writeline se deja una línea de por medio entre
+            //cada línea de instrucción y con write no deja ningun espacio entre las líneas de instrucción y muestra todas juntas. 
+
+            Console.Write("Hola Mundo ");
+            Console.Write("soy " + Nombre);
+            Console.ReadKey();
+
+            // El proyecto es un compilador porque es un programa que traduce un programa escrito en lenguaje fuente
+
+            //Análisis
+
+            // Entradas: Preguntar el nombre
+            // Procesos: Guardar el nombre en una variable para después mostrarlo
+            // Salidas: Mostrar el nombre con writeline y write
+
+             
+        }
+    }
+}
---
+++ 7.0.0/Laboratorio 1/T1_JRRA_108712/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace T1_JRRA_108712
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mi segundo programa");
+            Console.WriteLine("");
+
+            Console.WriteLine("Ingrese su nombre: ");
+            Console.WriteLine("");
+            string sNombre = Console.ReadLine();
+
+            Console.WriteLine("");
+            Console.WriteLine("Ingrese su edad: ");
+            Console.WriteLine("");
+            string sEdad = Console.ReadLine();
+
+            Console.WriteLine("");
+            Console.WriteLine("Ingrese su carrera: ");
+            Console.WriteLine("");
+            string sCarrera = Console.ReadLine();
+
+            Console.WriteLine("");
+            Console.WriteLine("Ingrese su número de carnet: ");
+            Console.WriteLine("");
+            string sCarné = Console.ReadLine();
+            Console.WriteLine("");
+
+            Console.WriteLine("Soy " + sNombre + " tengo " + sEdad + " años y estudio la carrera de " + sCarrera + ". Mi número de carnet es: " + sCarné);
+
+            Console.ReadKey();
+        }
+    }
+}
